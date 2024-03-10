39e9322c82c5917c00862f39a27d3c206ec780b9
kaidoj <jast112@gmail.com>
2024-03-10 09:11:57 +0100

    Added get crawl delay from robots.txt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use dotenv::dotenv;
 use log::info;
-use env_logger;
+use log::warn;
+use regex::Regex;
 
 
 #[tokio::main]
@@ -12,16 +13,49 @@ async fn load_sitemap(sitemap_location: &str) -> Result<String, Box<dyn Error>> 
     Ok(body)
 }
 
+#[tokio::main]
+async fn load_robots_txt(website_url: &str) ->  String {
+    let url: String = [website_url, "/robots.txt"].join("");
+    let resp = reqwest::get(url).await.unwrap();
+    let body = resp.text().await.unwrap();
+
+    return body
+}
+
+fn get_crawl_delay_from_robots_txt(website_url: &str ) -> Option<i8>  {
+    let body = load_robots_txt(website_url);
+    let re = Regex::new("Crawl-delay:s+(d+)$").unwrap();
+    let Some(matches) = re.captures(&body) else {
+        return None;
+    };
+
+    println!("Matches {:?}", matches);
+
+    return None;
+}
+
 
 fn main() {
     dotenv().ok();
     env_logger::init();
     let website_url: String = std::env::var("WEBSITE_URL").expect("WEBSITE_URL environment variable missing");
     let sitemap_location: String = std::env::var("SITEMAP_LOCATION").expect("SITEMAP_LOCATION environment variable missing");
+    let mut crawl_delay: i8 = std::env::var("CRAWL_DELAY").expect("CRAWL_DELAY environment variable missing").parse().unwrap();
 
     info!("Website url environment variable loaded {}", website_url);
     info!("Sitemap location environment variable loaded {}", sitemap_location);
 
+    let robots_txt_crawl_delay = get_crawl_delay_from_robots_txt(&website_url);
+    match robots_txt_crawl_delay {
+        Some(delay) => {
+            crawl_delay = delay;
+            info!("Found Crawl-delay from robots.txt {} seconds", crawl_delay);
+        },
+        None => {
+            warn!("robots.txt did not contain Crawl-delay. Using default {} seconds", crawl_delay);
+        }
+    }
+    
     let result = load_sitemap(&sitemap_location);
     info!("{:?}", result)
 }
